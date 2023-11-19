efd34e21ee74d6efc63ad60eb2b9862d3180397c
John Gabriel <accounts@johngabriel.co.uk>
2023-11-19 00:45:03 +0000

    Add music to level complete screen

--- game/scenes/level_complete.gd
+++ game/scenes/level_complete.gd
@@ -4,6 +4,7 @@ extends Control
 @onready var label_loot_value : Label = $GridContainer/Label_Loot_Value
 @onready var label_alerts_value : Label = $GridContainer/Label_Alerts_Value
 @onready var label_rank_value : Label = $GridContainer/Label_Rank_Value
+@onready var audio_bgm : AudioStreamPlayer = $Audio_BGM
 @onready var anim_player : AnimationPlayer = $AnimationPlayer
 
 enum State {IN, THERE, OUT}
@@ -28,4 +29,5 @@ func _ready() -> void:
 	label_time_taken_value.text = Utilities.int_to_timestamp(GameProgress.time_taken)
 	label_loot_value.text = "$" + str(GameProgress.loot)
 	label_alerts_value.text = str(GameProgress.camera_alerts)
+	audio_bgm.play()
 	anim_player.play("appear")
