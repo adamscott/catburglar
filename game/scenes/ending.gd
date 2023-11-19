1bd950cd710bd243da6b115fe3bdc990ffd46346
John Gabriel <accounts@johngabriel.co.uk>
2023-11-19 10:00:27 +0000

    Add ending cutscene

---
+++ game/scenes/ending.gd
@@ -0,0 +1,10 @@
+extends Control
+
+@onready var anim_player : AnimationPlayer = $AnimationPlayer
+
+func _on_animation_player_animation_finished(anim_name : String) -> void:
+	get_tree().change_scene_to_file("res://scenes/title_screen.tscn")
+
+func _ready() -> void:
+	await get_tree().create_timer(3.0).timeout
+	anim_player.play("ending")
